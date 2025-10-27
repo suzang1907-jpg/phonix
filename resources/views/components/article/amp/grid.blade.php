ab6010d0d2c5fa814e7a040b24e002762f108090
Suzan Gizem <suzang1907@gmail.com>
2025-10-27 21:34:37 +0300

    i

--- resources/views/components/article/amp/grid.blade.php
+++ resources/views/components/article/amp/grid.blade.php
@@ -14,17 +14,23 @@
                         href="{{ $article['url'] }}">
                         <div class="min-h-40 relative aspect-[8/12]">
                             <amp-img layout="fill" alt="{{ $article['article']['title'] }}"
-                                class="object-cover w-full h-full cover"
-                                src="{{ $article['image'] }}"></amp-img>
+                                class="object-cover w-full h-full cover" src="{{ $article['image'] }}"></amp-img>
                         </div>
 
                         <div class="absolute left-0 right-0 bottom-0 z-20">
                             <div class="relative py-4 flex flex-grow flex-col article-style-info">
                                 <div class="absolute inset-0 -z-10 article-style-layer"></div>
-                                <h2 class="font-bold text-center text-lg whitespace-nowrap">{{ $article['article']['title'] }}</h2>
+                                <h2 class="font-bold text-center text-lg whitespace-nowrap">
+                                    {{ $article['article']['title'] }}</h2>
                                 @unless (empty($article['number']))
                                     <p class="text-center font-bold text-lg whitespace-nowrap">
                                         {{ $article['number'] }}</p>
+                                @endunless
+                                @unless (empty($article['article']['info']))
+                                    <div class="absolute top-2 left-2 px-2 py-2 bg-black/50 rounded">
+                                        <p class="text-center text-sm whitespace-nowrap">
+                                            {{ $article['article']['info'] }}</p>
+                                    </div>
                                 @endunless
                             </div>
                         </div>
