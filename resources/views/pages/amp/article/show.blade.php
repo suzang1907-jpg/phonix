6da9c865450515901aad8dbe71830d6f24e1080e
Suzan Gizem <suzang1907@gmail.com>
2025-10-27 21:48:22 +0300

    improvements

--- resources/views/pages/amp/article/show.blade.php
+++ resources/views/pages/amp/article/show.blade.php
@@ -1,25 +1,37 @@
 <x-layout.amp :page="$page" :ampiframe="false">
     <div class="max-w-screen-sm mx-auto text-invert bg-background">
-        <x-link.phone :article="$article" />
-        <div class="pt-2"></div>
-        <x-link.whatsapp :article="$article" />
-        <div class="pt-2"></div>
-        <x-link.telegram :article="$article" />
+        @unless (empty($article->phone_number))
+            <x-link.phone :article="$article" />
+            <div class="pb-2"></div>
+        @endunless
+
+        @unless (empty($article->whatsapp_number))
+            <x-link.whatsapp :article="$article" />
+            <div class="pb-2"></div>
+        @endunless
+        @unless (empty($article->telegram))
+            <x-link.telegram :article="$article" />
+            <div class="pb-2"></div>
+        @endunless
         <article class="relative w-full text-white flex-col pt-12">
             <div class="relative aspect-square rounded-full mx-auto max-w-48">
-                <amp-img layout="fill" alt="{{ $article->title }}" class="object-cover w-full h-full cover amp-image-bottom-rounded-full" src="{{ route('file.article.main.image', ['id' => $article->id, 'xversion' => $article->image_id]) }}"></amp-img>
-                @if($article->isOnline() || true)
-                <div class="absolute bottom-4 right-4 rounded-full bg-background flex flex-col items-center justify-center p-0.5 h-6 w-6">
-                    <div class="bg-green-400 rounded-full h-full w-full">
+                <amp-img layout="fill" alt="{{ $article->title }}"
+                    class="object-cover w-full h-full cover amp-image-bottom-rounded-full"
+                    src="{{ route('file.article.main.image', ['id' => $article->id, 'xversion' => $article->image_id]) }}"></amp-img>
+                @if ($article->isOnline() || true)
+                    <div
+                        class="absolute bottom-4 right-4 rounded-full bg-background flex flex-col items-center justify-center p-0.5 h-6 w-6">
+                        <div class="bg-green-400 rounded-full h-full w-full">
 
+                        </div>
                     </div>
-                </div>
                 @else
-                <div class="absolute bottom-4 right-4 rounded-full bg-background flex flex-col items-center justify-center p-0.5 h-6 w-6">
-                    <div class="bg-gray-400 rounded-full h-full w-full">
+                    <div
+                        class="absolute bottom-4 right-4 rounded-full bg-background flex flex-col items-center justify-center p-0.5 h-6 w-6">
+                        <div class="bg-gray-400 rounded-full h-full w-full">
 
+                        </div>
                     </div>
-                </div>
                 @endif
             </div>
             <h2 class="text-lg font-bold text-center mt-2">{{ $article->title }}</h2>
