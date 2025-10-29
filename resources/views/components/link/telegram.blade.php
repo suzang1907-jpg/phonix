7b9562bcd93e4be7312c0a40fb0151db4a15056b
Suzan Gizem <suzang1907@gmail.com>
2025-10-29 21:46:19 +0300

    i

--- resources/views/components/link/telegram.blade.php
+++ resources/views/components/link/telegram.blade.php
@@ -1,7 +1,7 @@
 @props(['article' => null])
 <div>
     @unless (empty($article->telegram))
-    <a class="block px-3 py-2 bg-gradient-to-r from-blue-700 to-blue-600 text-white w-full link-telegram" target="_blank" href="https://t.me/{{ str_replace(' ', '', $article->telegram) }}?text={{ $article->getFormattedWhatsAppMessage() }}">
+    <a class="block px-3 py-2 bg-telegram-link text-white w-full link-telegram" target="_blank" href="https://t.me/{{ str_replace(' ', '', $article->telegram) }}?text={{ $article->getFormattedWhatsAppMessage() }}">
         <div class="w-full text-center flex flex-row items-center justify-evenly font-bold">
             <x-svg.telegram />
             <div>
