1d85a69c77a3f62cc34fa93a1855e6d03310fe05
Suzan Gizem <suzang1907@gmail.com>
2025-10-29 21:39:29 +0300

    improvements

--- resources/views/components/link/phone.blade.php
+++ resources/views/components/link/phone.blade.php
@@ -1,7 +1,7 @@
 @props(['article' => null])
 <div>
     @unless (empty($article->phone_number))
-        <a class="block px-3 py-2 bg-gradient-to-r from-gray-900 to-gray-800 text-white w-full link-phone" target="_blank"
+        <a class="block px-3 py-2 bg-phone-link text-white w-full link-phone" target="_blank"
             href="{{ $article->getFormattedPhoneNumberLink() }}">
             <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold">
                 <x-svg.phone />
--- resources/views/components/link/whatsapp-s.blade.php
+++ resources/views/components/link/whatsapp-s.blade.php
@@ -1,7 +1,7 @@
 @props(['article' => null])
 <div class="w-full h-full">
     @unless (empty($article->whatsapp_number))
-        <a class="block px-3 py-2 bg-gradient-to-r from-green-700 to-green-600 text-white w-full link-whatsapp" target="_blank"
+        <a class="block px-3 py-2 bg-whatsapp-link bg-gradient-to-r from-green-700 to-green-600 text-white w-full link-whatsapp" target="_blank"
             href="{{ $article->getFormattedWhatsappLink() }}">
             <div class="w-full text-center flex flex-row items-center justify-evenly font-bold">
                 <x-svg.whatsapp-s />
