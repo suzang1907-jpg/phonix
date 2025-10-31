75aa7b1d5a88f78cc8d4af5790b17e78691f78e8
Suzan Gizem <suzang1907@gmail.com>
2025-10-31 13:24:00 +0300

    i

--- resources/views/pages/customer/article/Edit.vue
+++ resources/views/pages/customer/article/Edit.vue
@@ -56,9 +56,9 @@
       </p>
     </div>
 
-    <bs-button-solid :onclick="this.update" class="mt-2">
+    <button v-on:click="this.update" class="btn btn-success mt-2">
       Kaydet
-    </bs-button-solid>
+    </button>
   </div>
 </template>
 <script>
