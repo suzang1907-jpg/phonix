1977abe93ff630563ec839eac0d415a0af3f7ea3
Suzan Gizem <suzang1907@gmail.com>
2025-11-04 20:34:30 +0300

    i

--- resources/views/components/blog/amp/grid/item.blade.php
+++ resources/views/components/blog/amp/grid/item.blade.php
@@ -1,12 +1,10 @@
 @props(['blog' => null])
 
-<article class="border border-base-300 rounded-md w-full h-full bg-base-200">
-    <div class="flex flex-col p-2">
-        <a class="font-bold link-blog-listener" href="{{ App\Routing\Amp::route('amp.blog.show', ['id' => $blog->id]) }}">
-            {{ strlen($blog->title) > 50 ? mb_substr($blog->title, 0, 50) . '...' : $blog->title }}
-        </a>
-        <p class="text-sm">
-            {{ $blog->short_description }}
-        </p>
-    </div>
+<article class="blog-item">
+    <a class="link-blog-listener" href="{{ App\Routing\Amp::route('amp.blog.show', ['id' => $blog->id]) }}">
+        {{ strlen($blog->title) > 50 ? mb_substr($blog->title, 0, 50) . '...' : $blog->title }}
+    </a>
+    <p>
+        {{ $blog->short_description }}
+    </p>
 </article>
