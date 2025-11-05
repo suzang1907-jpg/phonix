2883d2efe1cebf3bc938a8f24621026325bd5a6b
Suzan Gizem <suzang1907@gmail.com>
2025-11-05 22:01:40 +0300

    i

--- resources/views/components/link/whatsapp-s.blade.php
+++ resources/views/components/link/whatsapp-s.blade.php
@@ -1,5 +1,5 @@
 @props(['link' => null])
-<div class="w-half col-span-1 h-full">
+<div class="flex grow col-span-1 h-full">
     @unless (empty($link))
         <a class="block px-3 py-2 bg-whatsapp-link text-white w-full link-whatsapp h-full rounded-r-md" target="_blank" href="tel://{{ $link }}">
             <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold h-6">
