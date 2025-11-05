f8f4824d0ffa8d6bad5416897429f0cc74c0585e
Suzan Gizem <suzang1907@gmail.com>
2025-11-05 21:48:49 +0300

    i

--- resources/views/components/article/amp/grid.blade.php
+++ resources/views/components/article/amp/grid.blade.php
@@ -29,10 +29,10 @@
                                 @unless (empty($article['linkphone']) || empty($article['linkwhatsapp']))
                                     <div class="grid-cols-2 gap-1 w-full">
                                         @unless (empty($article['linkphone']))
-                                            <x-link.phone-s :value="$article['linkphone']" ></x-link.phone-s>
+                                            <x-link.phone-s :link="$article['linkphone']" ></x-link.phone-s>
                                         @endunless
                                         @unless (empty($article['linkwhatsapp']))
-                                            <x-link.whatsapp-s :value="$article['linkwhatsapp']" ></x-link.whatsapp-s>
+                                            <x-link.whatsapp-s :link="$article['linkwhatsapp']" ></x-link.whatsapp-s>
                                         @endunless
                                     </div>
                                 @endunless
--- resources/views/components/link/whatsapp-s.blade.php
+++ resources/views/components/link/whatsapp-s.blade.php
@@ -1,7 +1,7 @@
-@props(['value' => null])
+@props(['link' => null])
 <div class="w-full col-span-1">
-    @unless (empty($value))
-        <a class="block px-3 py-2 bg-whatsapp-link text-white w-full link-whatsapp" target="_blank" href="tel://{{ $value }}">
+    @unless (empty($link))
+        <a class="block px-3 py-2 bg-whatsapp-link text-white w-full link-whatsapp" target="_blank" href="tel://{{ $link }}">
             <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold">
                 <x-svg.whatsapp-s />
             </div>
