44bdb216cc24990cace51a1b96fa05fb5447191d
Suzan Gizem <suzang1907@gmail.com>
2025-11-05 19:48:20 +0300

    i

--- resources/views/components/navbar/amp.blade.php
+++ resources/views/components/navbar/amp.blade.php
@@ -1,9 +1,6 @@
-@props(['title' => null, 'page' => null])
-<header class="flex flex-row justify-between w-full items-center border-b p-2 border-b-base-300">
-    <div class="flex flex-row items-center justify-center w-full">
-        <h1 class="text-center text-base font-bold py-2">
-            {{ empty($title) ? App\Services\Page::current()->heading() : $title }}
-        </h1>
-    </div>
-    <div>{{ $slot }}</div>
+@props(['page' => null])
+<header>
+    <h1>
+        {{ trim(App\Services\Page::current()->heading()) }}
+    </h1>
 </header>
