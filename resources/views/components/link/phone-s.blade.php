40b84ba16010002d35c2ec5cea64f40b2a380a30
Suzan Gizem <suzang1907@gmail.com>
2025-11-05 22:02:50 +0300

    i

--- resources/views/components/link/phone-s.blade.php
+++ resources/views/components/link/phone-s.blade.php
@@ -1,7 +1,7 @@
 @props(['link' => null])
 <div class="flex grow col-span-1 h-full">
     @unless (empty($link))
-        <a class="block px-3 py-2 bg-phone-link text-white w-full link-phone h-full rounded-l-md" target="_blank" href="tel://{{ $link }}">
+        <a class="px-3 py-2 bg-phone-link text-white flex grow link-phone h-full rounded-l-md" target="_blank" href="{{ $link }}">
             <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold">
                 <x-svg.phone-s />
             </div>
--- resources/views/components/link/whatsapp-s.blade.php
+++ resources/views/components/link/whatsapp-s.blade.php
@@ -1,7 +1,7 @@
 @props(['link' => null])
 <div class="flex grow col-span-1 h-full">
     @unless (empty($link))
-        <a class="block px-3 py-2 bg-whatsapp-link text-white w-full link-whatsapp h-full rounded-r-md" target="_blank" href="tel://{{ $link }}">
+        <a class="flex grow px-3 py-2 bg-whatsapp-link text-white link-whatsapp h-full rounded-r-md" target="_blank" href="{{ $link }}">
             <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold h-6">
                 <x-svg.whatsapp-s />
             </div>
