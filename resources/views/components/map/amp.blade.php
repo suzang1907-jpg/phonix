7663d4993cd13f89fab2fdaf7eba29798593cacc
Suzan Gizem <suzang1907@gmail.com>
2025-11-05 20:15:26 +0300

    i

--- resources/views/components/map/amp.blade.php
+++ resources/views/components/map/amp.blade.php
@@ -13,12 +13,9 @@
 @if (empty($mapUrl))
     <div></div>
 @else
-    <div class="max-w-xl w-full mx-auto">
-        <amp-iframe
-            title="{{ empty($district?->district) ? __('location.' . $city->city) : __('location.' . $district->district) }}"
-            width="300" height="150" sandbox="allow-scripts allow-same-origin" layout="responsive"
-            src="{{ $mapUrl }}">
-        </amp-iframe>
-
-    </div>
+    <amp-iframe
+        title="{{ empty($district?->district) ? __('location.' . $city->city) : __('location.' . $district->district) }}"
+        width="300" height="150" sandbox="allow-scripts allow-same-origin" layout="responsive"
+        src="{{ $mapUrl }}">
+    </amp-iframe>
 @endif
