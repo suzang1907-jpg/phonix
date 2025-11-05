a1a99d3aa78eb71fbb27fb2ba2086752361812e9
Suzan Gizem <suzang1907@gmail.com>
2025-11-05 21:46:24 +0300

    i

--- resources/views/components/link/whatsapp-s.blade.php
+++ resources/views/components/link/whatsapp-s.blade.php
@@ -3,7 +3,7 @@
     @unless (empty($value))
         <a class="block px-3 py-2 bg-whatsapp-link text-white w-full link-whatsapp" target="_blank" href="tel://{{ $value }}">
             <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold">
-                <x-svg.whatsapp />
+                <x-svg.whatsapp-s />
             </div>
         </a>
     @endunless
