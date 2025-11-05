7d0101bade3e4db4e23ccac59701c062bdb2cc2d
Suzan Gizem <suzang1907@gmail.com>
2025-11-05 22:39:01 +0300

    i

--- resources/views/components/link/phone-s.blade.php
+++ resources/views/components/link/phone-s.blade.php
@@ -1,11 +1,9 @@
 @props(['link' => null, 'rounded' => false])
-<div class="flex grow col-span-1 h-full">
-    @unless (empty($link))
-        <a class="px-3 py-2 bg-phone-link text-white flex grow link-phone h-full rounded-l-md {{ $rounded ? 'rounded-r-md' : '' }}"
-            target="_blank" href="{{ $link }}">
-            <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold">
-                <x-svg.phone-s />
-            </div>
-        </a>
-    @endunless
-</div>
+@unless (empty($link))
+    <a class="px-3 py-2 bg-phone-link text-white flex grow link-phone h-full rounded-l-md {{ $rounded ? 'rounded-r-md' : '' }}"
+        target="_blank" href="{{ $link }}">
+        <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold">
+            <x-svg.phone-s />
+        </div>
+    </a>
+@endunless
--- resources/views/components/link/whatsapp-s.blade.php
+++ resources/views/components/link/whatsapp-s.blade.php
@@ -1,11 +1,9 @@
 @props(['link' => null, 'rounded' => false])
-<div class="flex grow col-span-1 h-full">
-    @unless (empty($link))
-        <a class="flex grow px-3 py-2 bg-whatsapp-link text-white link-whatsapp h-full rounded-r-md {{ $rounded ? 'rounded-l-md' : '' }}"
-            target="_blank" href="{{ $link }}">
-            <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold h-6">
-                <x-svg.whatsapp-s />
-            </div>
-        </a>
-    @endunless
-</div>
+@unless (empty($link))
+    <a class="flex grow px-3 py-2 bg-whatsapp-link text-white link-whatsapp h-full rounded-r-md {{ $rounded ? 'rounded-l-md' : '' }}"
+        target="_blank" href="{{ $link }}">
+        <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold h-6">
+            <x-svg.whatsapp-s />
+        </div>
+    </a>
+@endunless
