4fb0af95564c547c5c82566ae31704981488ff17
Suzan Gizem <suzang1907@gmail.com>
2025-11-05 21:46:28 +0300

    i

--- resources/views/components/link/phone-s.blade.php
+++ resources/views/components/link/phone-s.blade.php
@@ -3,7 +3,7 @@
     @unless (empty($value))
         <a class="block px-3 py-2 bg-phone-link text-white w-full link-phone" target="_blank" href="tel://{{ $value }}">
             <div class="py-1 w-full text-center flex flex-row items-center justify-evenly font-bold">
-                <x-svg.phone />
+                <x-svg.phone-s />
             </div>
         </a>
     @endunless
