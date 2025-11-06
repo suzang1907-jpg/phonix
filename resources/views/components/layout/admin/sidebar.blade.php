2a2a153bf7862ccaca8d131b48770ebeba1ba6af
Suzan Gizem <suzang1907@gmail.com>
2025-11-06 23:09:04 +0300

    i

--- resources/views/components/layout/admin/sidebar.blade.php
+++ resources/views/components/layout/admin/sidebar.blade.php
@@ -1,17 +1,15 @@
 @props(['page', 'title' => null])
 
-@if (false)
-    <components-admin-layout current="{{ url()->current() }}" title="{{ $page->title() }}"
-        project="{{ route('admin.project.edit', ['id' => $page->project()->id]) }}"
-        locations="{{ route('admin.location.list') }}" sitemaps="{{ route('admin.sitemap.list') }}"
-        tags="{{ route('admin.tag.list') }}" domains="{{ route('admin.domain.list') }}"
-        articles="{{ route('admin.article.list') }}" dashboard="{{ route('admin.dashboard.show') }}"
-        blogs="{{ route('admin.blog.list') }}" stories="{{ route('admin.story.list') }}"
-        workspace="{{ route('admin.workspace.show') }}" customers="{{ route('admin.customer.list') }}"
-        search="{{ route('admin.search.show') }}"
-        keywords="{{ route('admin.keyword.list') }}"></components-admin-layout>
-    <components-admin-alert-list></components-admin-alert-list>
-@endif
+<components-admin-layout current="{{ url()->current() }}" title="{{ $page->title() }}"
+    project="{{ route('admin.project.edit', ['id' => $page->project()->id]) }}"
+    locations="{{ route('admin.location.list') }}" sitemaps="{{ route('admin.sitemap.list') }}"
+    tags="{{ route('admin.tag.list') }}" domains="{{ route('admin.domain.list') }}"
+    articles="{{ route('admin.article.list') }}" dashboard="{{ route('admin.dashboard.show') }}"
+    blogs="{{ route('admin.blog.list') }}" stories="{{ route('admin.story.list') }}"
+    workspace="{{ route('admin.workspace.show') }}"
+    customers="{{ route('admin.customer.list') }}"
+    search="{{ route('admin.search.show') }}" keywords="{{ route('admin.keyword.list') }}"></components-admin-layout>
+<components-admin-alert-list></components-admin-alert-list>
 
 <div id="content" class="min-h-dvh flex flex-col bg-base-200" x-cloak>
     <div class="flex flex-row items-center justify-between p-6">
