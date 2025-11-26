fa7f73d85844fdf48a2ec8f6fc8e6662cb733b4d
Suzan Gizem <suzang1907@gmail.com>
2025-11-26 08:26:20 +0300

    i

--- app/Actions/Dashboard/DashboardStore.php
+++ app/Actions/Dashboard/DashboardStore.php
@@ -12,7 +12,17 @@ class DashboardStore extends Action
 {
     public function handle()
     {
-        $validator = Validator::make($this->data, [
+        $project_id = $this->getData('project_id');
+        $domain = $this->getData('domain');
+        $date = $this->getData('date');
+
+        $data = [
+            'project_id' => $project_id,
+            'domain' => $domain,
+            'date' => $date,
+        ];
+
+        $validator = Validator::make($data, [
             'project_id' => 'required|exists:projects,id',
             'domain' => 'required|string|unique:upcoming_domains,domain',
             'date' => 'required|date|after_or_equal:today',
@@ -23,9 +33,9 @@ class DashboardStore extends Action
         }
 
         UpcomingDomain::create([
-            'project_id' => $this->data['project_id'],
-            'domain' => $this->data['domain'],
-            'date' => Carbon::parse($this->data['date'])->format('Y-m-d'),
+            'project_id' => $data['project_id'],
+            'domain' => $data['domain'],
+            'date' => Carbon::parse($data['date'])->format('Y-m-d'),
         ]);
 
         return Admin::redirect('dashboard.show')->with('success', 'Upcoming domain added successfully!');
