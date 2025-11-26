e6625bd031ba44e724dc1ca329013dfdb00ce0da
Suzan Gizem <suzang1907@gmail.com>
2025-11-26 08:18:40 +0300

    i

--- app/Actions/Dashboard/DashboardShow.php
+++ app/Actions/Dashboard/DashboardShow.php
@@ -29,7 +29,7 @@ class DashboardShow extends Action
             'latest_customers' => Customer::latest()->with('articles')->list(3),
             'latest_articles' => Article::withoutGlobalScope(ArticleActiveScope::class)->with('analytics')->latest()->list(3),
             'upcoming_domains' => UpcomingDomain::orderBy('date', 'asc')->get(),
-            'projects' => Project::all(),
+            'projects' => [ProjectService::getProject()],
         ];
     }
 
